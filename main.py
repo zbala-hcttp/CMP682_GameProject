cfccb28786abe0bdb2176611450984cb5ca53b9d
Merve Nur Yılmaz <mnur.yilmaz@stm.com.tr>
2021-01-25 01:36:56 +0300

    depth limit and front end improvement

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ rows = 11
 cols = 11
 lives = 3
 lasers = 3
-whirlpools = 3
-bombs = 3
+blackholes = 3
+depth = 0
 
 
 class Game:
     def __init__(self):
         self.window = Tk()
-        self.window.title("Ship-Game")
+        self.window.title("Spaceship Game")
         self.canvas = Canvas(self.window, width=width_of_canvas, height=height_of_canvas)
         self.canvas.pack()
-        self.whirlpool_img = ImageTk.PhotoImage(Image.open(r"images/whirlpool.png"))
-        self.whirlpool_cells = []
-        self.blue_ship = BattleShip(459, 536, 6, 11, ImageTk.PhotoImage(Image.open(r"images/ship_blue.png")))
-        self.red_ship = BattleShip(459, 34, 6, 1, ImageTk.PhotoImage(Image.open(r"images/ship_red.png")))
+        self.blackhole_img = ImageTk.PhotoImage(Image.open(r"images/whirlpool.png"))
+        self.blackhole_cells = []
+        self.blue_ship = BattleShip(459, 536, 6, 11, ImageTk.PhotoImage(Image.open(r"images/spaceship.png")))
+        self.red_ship = BattleShip(459, 34, 6, 1, ImageTk.PhotoImage(Image.open(r"images/ai_spaceship.png")))
         self.player1 = Player("You", self.blue_ship)
         self.player2 = Player("AI", self.red_ship)
         self.turn = True
@@ -59,19 +59,19 @@ class Game:
         for i in range(rows+1):
             self.canvas.create_line(200, i * size_of_board / (rows+1) + 25, size_of_board + 150,
                                     i * size_of_board / (rows+1) + 25)
-        self.canvas.create_rectangle(400, 25, 550, 75, outline="red", width=1.5)
-        self.canvas.create_rectangle(400, 525, 550, 575, outline="blue", width=1.5)
+        self.canvas.create_rectangle(400, 25, 550, 75, outline="#C70039", width=1.5)
+        self.canvas.create_rectangle(400, 525, 550, 575, outline="#2980B9", width=1.5)
 
     def draw_story(self):
         self.canvas.create_rectangle(10, 25, 180, 575)
-        self.canvas.create_text(95, 38, anchor=CENTER, text="Story", fill="blue", font=("Purisa", 12))
+        self.canvas.create_text(95, 38, anchor=CENTER, text="Story", fill="#0E6655", font=("Purisa", 12, "bold"))
         self.canvas.create_line(10, 50, 180, 50)
-        self.canvas.create_text(13, 55, anchor=NW, fill="green", font=("Purisa", 11),
-                                text="Reach to the opposite\nport before your enemy\nreaches to yours\nand win the game!"
-                                     "\n\nWatch out for your\nenemy! He can send\nwhirpools to your way or\nhe can sink your "
-                                     "ship\nusing lasers.\n\nEach player has 3 lasers\nand 5 whirlpool attacks.\n"
+        self.canvas.create_text(13, 55, anchor=NW, fill="#138D75", font=("Purisa", 11),
+                                text="Reach to the opposite\nhome before your enemy\nreaches to yours\nand win the game!"
+                                     "\n\nWatch out for your\nenemy! He can send\nblackholes to your way or\nhe can sink your "
+                                     "ship\nusing lasers.\n\nEach player has 3 lasers\nand 5 blackhole attacks.\n"
                                      "Lasers can be used\nwithin 3 blocks range.\n"
-                                     "Ships can survive only\n3 laser attacks."
+                                     "Spaceships can survive\nonly 3 laser attacks."
                                      "\n\nGood luck!")
 
     def draw_warnings(self):
@@ -79,25 +79,25 @@ class Game:
         turn = "AI's turn"
         if self.turn:
             turn = "Your turn"
-        self.canvas.create_text(475, 610, anchor=CENTER, text=turn, fill="magenta", font=("Purisa", 12))
+        self.canvas.create_text(475, 610, anchor=CENTER, text=turn, fill="#D81B60", font=("Purisa", 12))
         self.canvas.create_text(203, 620, anchor=NW, text="Warning >> " + self.warning_text,
-                                fill="red", font=("Purisa", 12))
+                                fill="#CB4335", font=("Purisa", 12, "bold"))
 
     def draw_player2_info(self):
         self.canvas.create_rectangle(770, 25, 990, 275)
-        self.canvas.create_text(880, 38, anchor=CENTER, text=self.player2.name, fill="red", font=("Purisa", 12))
+        self.canvas.create_text(880, 38, anchor=CENTER, text=self.player2.name, fill="#BF0238", font=("Purisa", 12, "bold"))
         self.canvas.create_line(770, 50, 990, 50)
-        self.canvas.create_text(773, 55, anchor=NW, fill="red", font=("Purisa", 11),
+        self.canvas.create_text(773, 55, anchor=NW, fill="#C70039", font=("Purisa", 11),
                                 text="lives : " + str(self.player2.lives) +
-                                     "\nwhirlpools : " + str(self.player2.ship.whirlpools))
+                                     "\nblackholes : " + str(self.player2.ship.blackholes))
 
     def draw_player1_info(self):
         self.canvas.create_rectangle(770, 325, 990, 575)
-        self.canvas.create_text(880, 338, anchor=CENTER, text=self.player1.name, fill="blue", font=("Purisa", 12))
+        self.canvas.create_text(880, 338, anchor=CENTER, text=self.player1.name, fill="#1B618E", font=("Purisa", 12, "bold"))
         self.canvas.create_line(770, 350, 990, 350)
-        self.canvas.create_text(773, 355, anchor=NW, fill="blue", font=("Purisa", 11),
+        self.canvas.create_text(773, 355, anchor=NW, fill="#2471A3", font=("Purisa", 11),
                                 text="lives : " + str(self.player1.lives) +
-                                     "\nwhirlpools : " + str(self.player1.ship.whirlpools))
+                                     "\nblackholes : " + str(self.player1.ship.blackholes))
 
     def place_ships(self):
         self.canvas.create_image(self.player1.ship.position_x, self.player1.ship.position_y, anchor=NW,
@@ -113,18 +113,18 @@ class Game:
         self.draw_warnings()
         self.draw_player1_info()
         self.draw_player2_info()
-        for i in range(len(self.whirlpool_cells)):
-            self.canvas.create_image((self.whirlpool_cells[i][0] - 1) * 50 + 209,
-                                     (self.whirlpool_cells[i][1] - 1) * 50 + 34,
-                                     anchor=NW, image=self.whirlpool_img)
+        for i in range(len(self.blackhole_cells)):
+            self.canvas.create_image((self.blackhole_cells[i][0] - 1) * 50 + 209,
+                                     (self.blackhole_cells[i][1] - 1) * 50 + 34,
+                                     anchor=NW, image=self.blackhole_img)
 
     def new_game(self):
         self.turn = True
         self.player1.lives = lives
-        self.player1.ship = BattleShip(459, 536, 6, 11, ImageTk.PhotoImage(Image.open(r"images/ship_blue.png")))
+        self.player1.ship = BattleShip(459, 536, 6, 11, ImageTk.PhotoImage(Image.open(r"images/spaceship.png")))
         self.player2.lives = lives
-        self.player2.ship = BattleShip(459, 34, 6, 1, ImageTk.PhotoImage(Image.open(r"images/ship_red.png")))
-        self.whirlpool_cells = []
+        self.player2.ship = BattleShip(459, 34, 6, 1, ImageTk.PhotoImage(Image.open(r"images/ai_spaceship.png")))
+        self.blackhole_cells = []
 
     def is_position_valid(self, x, y):
         if 200 <= x <= 750 and 25 <= y <= 575:
@@ -133,8 +133,8 @@ class Game:
     def is_cell_empty(self, x, y):
         if self.player2.ship.cell_x == x and self.player2.ship.cell_y == y:
             return False
-        for i in range(len(self.whirlpool_cells)):
-            if self.whirlpool_cells[i][0] == x and self.whirlpool_cells[i][1] == y:
+        for i in range(len(self.blackhole_cells)):
+            if self.blackhole_cells[i][0] == x and self.blackhole_cells[i][1] == y:
                 return False
         return True
 
@@ -257,18 +257,18 @@ class Game:
             self.has_player1_won()
             self.has_player2_won()
 
-    def put_whirlpool(self, player, x, y):
+    def put_blackhole(self, player, x, y):
         if player.is_out_of_lives():
             return False
         if (x == self.player1.ship.cell_x and y == self.player1.ship.cell_y)\
                 or (x == self.player2.ship.cell_x and y == self.player2.ship.cell_y):
             return False
         else:
-            if player.ship.whirlpools > 0:
-                for i in range(len(self.whirlpool_cells)):
-                    if self.whirlpool_cells[i][0] == x and self.whirlpool_cells[i][1] == y:
+            if player.ship.blackholes > 0:
+                for i in range(len(self.blackhole_cells)):
+                    if self.blackhole_cells[i][0] == x and self.blackhole_cells[i][1] == y:
                         return False
-                player.ship.whirlpools -= 1
+                player.ship.blackholes -= 1
                 return True
             return False
 
@@ -280,30 +280,35 @@ class Game:
             if (xc == self.player1.ship.cell_x and yc == self.player1.ship.cell_y)\
                     or (xc == self.player2.ship.cell_x and yc == self.player2.ship.cell_y)\
                         or (not self.is_position_valid(event.x, event.y)):
-                self.warning_text = "Illegal place to send whirlpool! Please send another cell."
+                self.warning_text = "Illegal place to send blackhole! Please send another cell."
             else:
-                if self.player1.ship.whirlpools > 0:
-                    for i in range(len(self.whirlpool_cells)):
-                        if self.whirlpool_cells[i][0] == xc and self.whirlpool_cells[i][1] == yc:
-                            self.warning_text = "There is already a whirlpool here!"
+                if self.player1.ship.blackholes > 0:
+                    for i in range(len(self.blackhole_cells)):
+                        if self.blackhole_cells[i][0] == xc and self.blackhole_cells[i][1] == yc:
+                            self.warning_text = "There is already a blackhole here!"
                             self.update_board()
                             return
                     self.warning_text = ""
-                    self.whirlpool_cells.append((xc, yc))
-                    self.player1.ship.whirlpools -= 1
+                    self.blackhole_cells.append((xc, yc))
+                    self.player1.ship.blackholes -= 1
                     self.turn = False
                     self.update_board()
                     self.play()
                 else:
-                    self.warning_text = "You have run out of whirlpools!"
+                    self.warning_text = "You have run out of blackholes!"
                     self.update_board()
         else:
             self.warning_text = "It is AI's turn!"
             self.update_board()
 
+    def calculate_euclidean_distance_of_ships(self):
+        distance = math.sqrt(sum((px - qx) ** 2.0 for px, qx
+                                 in zip([self.player1.ship.position_x, self.player1.ship.position_y],
+                                        [self.player2.ship.position_x, self.player2.ship.position_y])))
+        return distance
+
     def is_close(self, from_player, to_player):
-        if math.dist([from_player.ship.position_x, from_player.ship.position_y],
-                     [to_player.ship.position_x, to_player.ship.position_y]) < 200 \
+        if self.calculate_euclidean_distance_of_ships() < 200 \
                         and from_player.ship.has_lasers:
             return True
         return False
@@ -322,13 +327,16 @@ class Game:
         action = ""
         cell_x = 0
         cell_y = 0
-
+        global depth
+        depth += 1
         result = self.has_ended()
 
         if result == "AI" or self.player1.is_out_of_lives():
             return (1, "nothing", 0, 0)
         elif result == "You" or self.player2.is_out_of_lives():
             return (-1, "nothing", 0, 0)
+        elif depth >= 1000:
+            return (0, "nothing", 0, 0)
 
         if self.is_close(self.player2, self.player1):
             print("laser AI")
@@ -373,17 +381,17 @@ class Game:
                 if (j == 1 and (i == 5 or i == 6 or i == 7)) or \
                         (j == 11 and (i == 5 or i == 6 or i == 7)):
                     continue
-                if self.put_whirlpool(self.player2, i, j):
-                    self.whirlpool_cells.append((i, j))
-                    action = "whirlpool"
+                if self.put_blackhole(self.player2, i, j):
+                    self.blackhole_cells.append((i, j))
+                    action = "blackhole"
                     (m, a, x, y) = self.min_alpha_beta(alpha, beta)
                     if m > maxv:
                         maxv = m
                         action = a
                         cell_x = x
                         cell_y = y
-                    self.whirlpool_cells.pop()
-                    self.player2.ship.whirlpools += 1
+                    self.blackhole_cells.pop()
+                    self.player2.ship.blackholes += 1
                     if maxv >= beta:
                         return (maxv, action, cell_x, cell_y)
                     if maxv > alpha:
@@ -396,11 +404,15 @@ class Game:
         action = ""
         x = 0
         y = 0
+        global depth
+        depth += 1
         result = self.has_ended()
         if result == "AI":
             return (1, "nothing", 0, 0)
         elif result == "You":
             return (-1, "nothing", 0, 0)
+        elif depth >= 1000:
+            return (0, "nothing", 0, 0)
 
         if self.is_close(self.player1, self.player2):
             action = "laser"
@@ -439,17 +451,17 @@ class Game:
 
         for i in range(1, 12):
             for j in range(1, 12):
-                if self.put_whirlpool(self.player1, i, j):
-                    action = "whirlpool"
-                    self.whirlpool_cells.append((i, j))
+                if self.put_blackhole(self.player1, i, j):
+                    action = "blackhole"
+                    self.blackhole_cells.append((i, j))
                     (m, a, x, y) = self.max_alpha_beta(alpha, beta)
                     if m < minv:
                         minv = m
                         action = a
                         cell_x = x
                         cell_y = y
-                    self.whirlpool_cells.pop()
-                    self.player1.ship.whirlpools += 1
+                    self.blackhole_cells.pop()
+                    self.player1.ship.blackholes += 1
                     if minv <= alpha:
                         return (minv, action, cell_x, cell_y)
                     if minv < beta:
@@ -463,8 +475,8 @@ class Game:
             self.send_laser(self.player2, self.player1)
         elif action == "move":
             self.move(self.player2, x, y)
-        elif action == "whirlpool":
-            self.put_whirlpool(self.player2, x, y)
+        elif action == "blackhole":
+            self.put_blackhole(self.player2, x, y)
         self.turn = True
         self.update_board()
         self.has_player2_won()
@@ -481,7 +493,7 @@ class BattleShip:
         self.cell_x = cell_x
         self.cell_y = cell_y
         self.lasers = lasers
-        self.whirlpools = whirlpools
+        self.blackholes = blackholes
         self.image = image
 
     def has_lasers(self):
